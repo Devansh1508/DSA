4e029070ae780a9a4f3d0a7c7added4b005b27cf
Devansh <devanshpratap1500@gmail.com>
2025-03-22 23:13:37 +0530

    70 done

---
+++ 70. Climbing Stairs.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+    public:
+        vector <int>dp;
+        int solve(int n){
+            if(n<=0)return 0;
+            if(n==1)return 1;
+            if(n==2)return 2;
+
+            if(dp[n]!=-1)return dp[n];
+
+            int ans=0;
+            ans=solve(n-1)+solve(n-2);
+            return dp[n]=ans;
+        }
+
+        int climbStairs(int n) {
+            dp.resize(n+1,-1);
+            return solve(n);
+        }
+    };
